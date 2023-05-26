328a7d7ccd96aee51190d00cc0a08d28bc67d937
ada <amin_deldari@hotmail.com>
2023-05-26 19:30:38 +0300

    Initial commit

--- Part1_IntroductionToProgrammingExercises/Exercise_004.py
+++ Part1_IntroductionToProgrammingExercises/Exercise_004.py
@@ -5,3 +5,20 @@ Exercise 4:Area of a Field
     feet. Display the area of the field in acres.
         Hint: There are 43,560 square feet in an acre.
 """
+# Since there are 43560 square feet in an acre, and it is constant number
+# We can define a variable for this, which its name should be in uppercase.
+SQFT_PER_ACRE = 43560
+
+# Get information from user
+length = float(input("Enter the length of the field in feet: "))
+width = float(input("Enter the width of the field in feet: "))
+
+# Compute the area of the field in square feet
+area_in_sqft = length * width
+
+# Convert the area in square feet to acre
+area_in_acre = area_in_sqft / SQFT_PER_ACRE
+
+# Print the result
+print(f"The area of the field is {area_in_sqft} square feet or {area_in_acre} acres.")
+
--- Part1_IntroductionToProgrammingExercises/Exercise_005.py
+++ Part1_IntroductionToProgrammingExercises/Exercise_005.py
@@ -11,3 +11,29 @@ Exercise 5: Bottle Deposits
     received for returning those containers. Format the output so that it includes a dollar
     sign and always displays exactly two decimal places.
 """
+
+# In this exercise, we have two constants, one for containers equal or less than 1 liter
+# and the other for the containers holding more than one liter
+LESS_DEPOSIT = 0.10
+MORE_DEPOSIT = 0.25
+
+# Get the input from user
+less_than_one = float(input("How many containers 1 liter or less do you have? "))
+more_than_one = float(input("How many containers more than 1 liter do you have? "))
+
+# Compute the refunds
+refund_less = less_than_one * LESS_DEPOSIT
+refund_more = more_than_one * MORE_DEPOSIT
+
+total_refund = refund_less + refund_more
+
+# You can compute these in one line, but in future you may need just one of them, and
+# computing separately is wise choice, and in this way it is simple to read.
+# refund = less_than_one * LESS_DEPOSIT + more_than_one * MORE_DEPOSIT
+
+# Display the result
+print("Your total refund will be $%.2f" % total_refund)
+
+# The %.2f format specifier indicates that a value should be formatted as a floating point
+# number with 2 digits to the right of the decimal.
+
