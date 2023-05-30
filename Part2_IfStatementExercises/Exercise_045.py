f9f198fa747fbd8a94fdac49011941a6764e8274
ada <amin_deldari@hotmail.com>
2023-05-30 22:45:37 +0300

    Modification.

---
+++ Part2_IfStatementExercises/Exercise_045.py
@@ -0,0 +1,24 @@
+"""
+Exercise 45:What Color is that Square?
+
+    Positions on a chess board are identified by a letter and a number. The letter identifies
+    the column, while the number identifies the row, as shown below:
+
+            8   W   B   W   B   W   B   W   B
+            7   B   W   B   W   B   W   B   W
+            6   W   B   W   B   W   B   W   B
+            5   B   W   B   W   B   W   B   W
+            4   W   B   W   B   W   B   W   B
+            3   B   W   B   W   B   W   B   W
+            2   W   B   W   B   W   B   W   B
+            1   B   W   B   W   B   W   B   W
+                a   b   c   d   e   f   g   h
+
+    Write a program that reads a position from the user. Use an if statement to deter-
+    mine if the column begins with a black square or a white square. Then use modular
+    arithmetic to report the color of the square in that row. For example, if the user enters
+    a1 then your program should report that the square is black. If the user enters d5
+    then your program should report that the square is white. Your program may assume
+    that a valid position will always be entered. It does not need to perform any error
+    checking.
+"""
