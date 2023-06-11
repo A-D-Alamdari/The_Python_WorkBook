950d65d9180a29822dfb9ce127021b173c4990e5
Amin D. Alamdari <amin_deldari@hotmail.com>
2023-06-11 16:01:33 +0300

    Updates!

---
+++ Part6_DictionaryExercises/Exercise_134.py
@@ -0,0 +1,7 @@
+"""
+Exercise 134: Unique Characters
+
+    Create a program that determines and displays the number of unique characters in a
+    string entered by the user. For example, Hello, World! has 10 unique characters
+    while zzzhas only one unique character. Use a dictionary or set to solve this problem.
+"""
